9a2869f289902dbc9be2b123febde43e5dddbfad
Relintai <relintai@gmail.com>
2019-07-05 16:41:48 +0200

    Work on a new editor plugin.

--- register_types.cpp
+++ register_types.cpp
@@ -1,9 +1,23 @@
 #include "register_types.h"
 
 #include "touch_button.h"
+#include "mesh_data_resource.h"
+
+#ifdef TOOLS_ENABLED
+#include "editor/editor_plugin.h"
+
+#include "editor_plugin_collada_mdr.h"
+#endif
 
 void register_ui_extensions_types() {
 	ClassDB::register_class<TouchButton>();
+	ClassDB::register_class<MeshDataResource>();
+
+	#ifdef TOOLS_ENABLED
+	//ClassDB::register_class<EditorPluginColladaMdr>();
+
+	//EditorPlugins::add_by_type<EditorPluginColladaMdr>();
+	#endif
 }	
 
 void unregister_ui_extensions_types() {
